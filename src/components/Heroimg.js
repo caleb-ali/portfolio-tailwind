04aa4985f5fc85b986c5ad9e53aa1a3f8b4a2f5b
caleb-ali <slimkally@gmail.com>
2023-01-11 01:10:12 +0100

    added download link

--- src/components/Heroimg.js
+++ src/components/Heroimg.js
@@ -9,12 +9,14 @@ const Heroimg = () => {
        <div className="text-center lg:text-start text-3xl lg:text-5xl  text-gray-900 lg:mt-48 ">
         <h1> <span className="text-persian-green-600">Hi.</span> I'm Caleb</h1>
         <h2> A Frontend developer</h2>
-        <button
-            href="#"
+        <a
+            href="https://drive.google.com/file/d/1-O1_tUE9z3_90ILdLSgW4c4JCZ8lO7eV/view?usp=sharing"
+            target="_blank"
+              rel="noreferrer"
             class="mt-2 text-sm px-4 py-2 rounded  bg-persian-green-500 hover:bg-persian-green-600 text-white font-normal "
           >
             Download resume 
-          </button>
+          </a>
           <Link to="projects" spy={true} smooth={true} offset={-70} duration={500} className="mt-2 text-sm  ml-4 px-4 py-2 rounded text-persian-green-600 font-normal border-[1px] hover:bg-persian-green-500 hover:border-[0px] hover:text-white">
             {/*
           <button
